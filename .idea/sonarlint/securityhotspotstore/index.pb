48acc2f3266ad96116b4f4bfe7c56f470a804915
EriqueRocha <eriquebit@gmail.com>
2024-04-25 17:59:18 -0300

    Initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+=
+conversor.iml,d\c\dca952b41f5c8fdaddb88c43dbe8e497926937bd
---
+++ src/Main.java
@@ -0,0 +1,96 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final String apiKey = "5e93c796601871923fa0e130";
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("-----------------------");
+        System.out.println("| Conversor de moedas |");
+        System.out.println("-----------------------");
+        System.out.println("");
+        System.out.println("------ EXEMPLOS DE CÓDIGOS DE MOEDAS -------");
+        System.out.println("|        BRL--> REAL, USD--> DOLLAR        |");
+        System.out.println("--------------------------------------------");
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Digite '0'(zero) para encerrar o programa");
+
+            System.out.println("Digite o código da moeda de referência");
+
+            String moedaReferencia = scanner.nextLine().toUpperCase();
+
+            if (moedaReferencia.equals("0")) {
+                break;
+            }
+
+            System.out.println("Moeda de referência: " + moedaReferencia);
+
+            System.out.println("Digite a quantidade da moeda de referência");
+
+            String valor = "";
+            boolean entradaValida = false;
+
+            while (!entradaValida) {
+                valor = scanner.nextLine();
+
+                try {
+                    float numero = Float.parseFloat(valor);
+                    entradaValida = true;
+
+                    System.out.println("Você digitou: " + numero);
+                } catch (NumberFormatException e) {
+
+                    System.out.println("Entrada inválida. Por favor, digite um valor numérico válido.");
+                }
+            }
+
+            System.out.println("Valor: "+ moedaReferencia+" - "+ valor);
+
+            System.out.println("Digite o código da moeda que deseja realizar a conversão");
+
+            String moedaConversao = scanner.nextLine().toUpperCase();
+
+            System.out.println("Converter para: " + moedaConversao);
+
+            String url_str = "https://v6.exchangerate-api.com/v6/"+apiKey+"/pair/"+moedaReferencia+"/"+moedaConversao+"/"+valor.toString();
+
+            URL url = new URL(url_str);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+            int responseCode = request.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                JsonParser jp = new JsonParser();
+                JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+                JsonObject jsonobj = root.getAsJsonObject();
+
+                String moedaFinal = jsonobj.get("conversion_result").getAsString();
+                System.out.println("------------------------------------");
+                System.out.println("moeda de referência: "+moedaReferencia+" - " + valor);
+                System.out.println("moeda de conversão: "+moedaConversao+" - " + moedaFinal);
+                System.out.println("------------------------------------");
+
+            }else {
+                System.out.println("###############################");
+                System.out.println("| Código de moeda inexistente |");
+                System.out.println("###############################");
+                System.out.println("REVISE O CÓDIGO DA MOEDA FORNECIDA E TENTE NOVAMENTE");
+                System.out.println("-------------------------------------------------------");
+            }
+        }
+    }
+}
